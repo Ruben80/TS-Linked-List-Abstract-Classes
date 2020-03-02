4845fb222a778870d4f589d4e2f9ba03d8f943b3
Ruben80 <43403734+Ruben80@users.noreply.github.com>
2020-03-02 15:53:21 +0100

    Add files via upload

---
+++ src/Sorter.ts
@@ -0,0 +1,25 @@
+export abstract class Sorter {
+    abstract compare(leftIndex: number, rightIndex: number): boolean;
+    abstract swap(leftIndex: number, rightIndex: number): void;
+    abstract length: number;
+
+    // equivalent
+    // collection: number [];
+
+    // constructor(collection: number[]) {
+    //     this.collection = collection;
+    // } 
+
+    sort(): void {
+        const { length } = this;
+
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+
+                if (this.compare(j, j + 1)) {
+                    this.swap(j, j + 1);
+                }
+            }
+        }
+    }
+}
